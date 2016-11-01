4117f44643b4271f79d2db21271086c90fed6ced
Eren Aydemir <eren.aydemir@gmail.com>
2016-11-01 23:20:17 +0200

    chessboard dimensions are corrected

--- distortion.py
+++ distortion.py
@@ -7,8 +7,8 @@ import os
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((6*7,3), np.float32)
-objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+objp = np.zeros((6*9,3), np.float32)
+objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)
 
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
@@ -23,25 +23,23 @@ types = ('*.jpg', '*.JPG')
 images = []
 for files in types:
     images.extend(glob.glob(files))
-    
-i=0
 
 for fname in images:
     img = cv2.imread(fname)
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
     # Find the chess board corners
-    ret, corners = cv2.findChessboardCorners(gray, (7,6),None)
+    ret, corners = cv2.findChessboardCorners(gray, (9,6),None)
     
     # If found, add object points, image points (after refining them)
     if ret == True:
         objpoints.append(objp)
-
+        
         cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
         imgpoints.append(corners)
 
         # Draw and display the corners
-        cv2.drawChessboardCorners(img, (7,6), corners,ret)
+        cv2.drawChessboardCorners(img, (9,6), corners,ret)
         
         print fname
         cv2.imshow('img',img)
